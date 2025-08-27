71f10ede60e7132ed7947ab57660e5b36007fcc8
Akashsivv0 <Akashsivv@gmail.com>
2025-08-27 22:21:46 +0530

    changes for 27th aug main

--- backend/recommender.py
+++ backend/recommender.py
@@ -62,7 +62,7 @@ def recommend_jobs(user_skills, top_n=5):
 def add_to_recently_viewed(job_id):
     """Adds a job ID to the front of the queue."""
     # Add the ID to the queue
-    recently_viewed_queue.append(job_id)
+    recently_viewed_queue.append(job_id) #the queue function
 
 def get_recently_viewed():
     """Returns the jobs from the queue in order from newest to oldest."""
@@ -102,4 +102,4 @@ if __name__ == "__main__":
             else:
                 print("No jobs found or no matches could be made with the provided dataset.")
         except ValueError as e:
-            print(e)
+            print(e) 
